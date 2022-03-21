63100b2cd9a2a008ad7c26952cd227221419f170
ComPasser <zhao17335851220@163.com>
2022-03-21 19:25:32 +0800

    2022/3/21下

--- scanf.c
+++ scanf.c
@@ -10,11 +10,13 @@ int Add(int x,int y)
 
 int main()
 {
+	double d = 12.68;
 	//选择函数
 	int input = 0;
 	int line = 0;
 	int com = 0;
-	int arr[10];
+	int arr[10] = {0};
+	int a = 0;
 	printf("shijiannihao\n");
 	printf("你要好好学习嘛（0/1）：");
 	scanf("%d",&input);
@@ -31,19 +33,21 @@ int main()
 		}
 	}
 	else
+	{
 		printf("NO offer\n");
+	}
 	printf("请输入第一个整数：");
 	scanf("%d", &input);
 	printf("请输入第二个整数：");
 	scanf("%d", &com);
 	printf("这两个整数的和为：%d\n", Add(input, com));
 
-	printf("请输入10个整数：");
-	for (int i = 0;i < 10;i++)
-	{
-		scanf("%d",&arr[i]);
+	//printf("请输入10个整数：");
+	//for (int i = 0;i < 10;i++)
+	//{
+	//	scanf("%d",&arr[i]);
 
-	}
+	//}
 	for (int i = 0; i < 10; i++)
 	{
 		printf("%d ", arr[i]);
@@ -51,5 +55,22 @@ int main()
 
 	printf("\n数组的大小为：%d\n",sizeof(arr));   //数组的总大小 
 	printf("数组的个数为：%d\n",sizeof(arr)/sizeof(arr[0]));
+	
+	//按位取反  ~
+	printf("按位取反的值为：%d\n",~a);
+	//强制类型转换
+	printf("%d\n",(int)d);   //将12.68强转为int  损失精度
+
+	//三目操作符
+	printf("输入比较的第一个数：");
+	scanf("%d", &input);
+	printf("输入比较的第一个数：");
+	scanf("%d", &com);
+
+	printf("较大的数为：%d\n",(input > com ? input : com));
+
+	//类型重命名  typedef
+
+	
 	return 0;
 }
